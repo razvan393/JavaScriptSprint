0a93f786071cf9d50f3161b4a712b643da521a99
razvan393 <razvanghinea05@gmail.com>
2015-12-16 13:55:50 +0200

    assignment6.3

--- MicroAssignments/Batch1/7/script/main.js
+++ MicroAssignments/Batch1/7/script/main.js
@@ -2,18 +2,16 @@
 
 function verificare() {
 var nume = document.getElementById("name").value;
-nume = nume.trim();
 var vector = nume.split(" ");
-console.log(vector.length);
-if (vector.length === 2)
+if (vector.length === 2) 
 {
 	if((vector[0].length >=3) && (vector[1].length >=3))
 	{
-		document.getElementById("container").classList.add("valid");
+		document.getElementById("container").className = "valid";
 	}
 	else
 	{
-		document.getElementById("container").class
+		document.getElementById("container").className = "";
 	}
 }
 else 
