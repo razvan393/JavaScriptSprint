825cd42bb0f0a39b20131aff91938cf88ee63550
razvan393 <razvanghinea05@gmail.com>
2015-12-16 13:36:09 +0200

    assignment6

--- MicroAssignments/Batch1/6/script/main.js
+++ MicroAssignments/Batch1/6/script/main.js
@@ -1,2 +1,35 @@
-var nume = document.getElementById("name").value;
+var k = 0;
 
+function verificare() {
+var nume = document.getElementById("name").value;
+var vector = nume.split(" ");
+if (vector.length === 2) 
+{
+	if((vector[0].length >=3) && (vector[1].length >=3))
+	{
+		k = 1;
+	}
+	else
+	{
+		k = 0;
+	}
+}
+else 
+{
+	k = 0;
+}
+
+if(k === 1)
+{
+	document.getElementById("container").className = "valid";
+}
+else
+{
+	document.getElementById("container").className = "";
+
+}
+
+}
+
+var start = document.getElementById("name");
+start.addEventListener("input", verificare, false);
