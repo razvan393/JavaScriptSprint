0cb675163f22d89ad376ecd9a60944ec5fedf7bb
razvan393 <razvanghinea05@gmail.com>
2015-12-16 16:25:07 +0200

    assignment7

--- MicroAssignments/Batch1/7/script/main.js
+++ MicroAssignments/Batch1/7/script/main.js
@@ -1,25 +1,16 @@
-
-
-function verificare() {
-var nume = document.getElementById("name").value;
-var vector = nume.split(" ");
-if (vector.length === 2) 
-{
-	if((vector[0].length >=3) && (vector[1].length >=3))
+function messaging (evt) {
+	evt.preventDefault();
+	var mesaj = document.getElementById("msg").value;
+	var locMesaj = document.getElementById("msg")
+	var contain = document.getElementById("chat");
+	if(mesaj.length > 0)
 	{
-		document.getElementById("container").className = "valid";
+	contain.insertAdjacentHTML("beforeend","<div><img src='images/images.png' /><div class='add'><p>"+mesaj+"</p><span>John Dow</span></div></div>");
+	locMesaj.value = ""; 
 	}
-	else
-	{
-		document.getElementById("container").className = "";
-	}
-}
-else 
-{
-	document.getElementById("container").className = "";
-}
 
 }
 
-var start = document.getElementById("name");
-start.addEventListener("input", verificare, false);
+
+var buton = document.getElementById("form1");
+buton.addEventListener("submit", messaging, false);
