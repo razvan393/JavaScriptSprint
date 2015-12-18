a9d1ec35a01b06d4a2dede626fb7ba02d157bcd3
razvan393 <razvanghinea05@gmail.com>
2015-12-18 11:02:19 +0200

    10.3

--- MicroAssignments/Batch1/10/script/main.js
+++ MicroAssignments/Batch1/10/script/main.js
@@ -1,3 +1,41 @@
+// Simple JavaScript Templating
+// John Resig - http://ejohn.org/ - MIT Licensed
+(function(){
+  var cache = {};
+ 
+  this.tmpl = function tmpl(str, data){
+    // Figure out if we're getting a template, or if we need to
+    // load the template - and be sure to cache the result.
+    var fn = !/\W/.test(str) ?
+      cache[str] = cache[str] ||
+        tmpl(document.getElementById(str).innerHTML) :
+     
+      // Generate a reusable function that will serve as a template
+      // generator (and which will be cached).
+      new Function("obj",
+        "var p=[],print=function(){p.push.apply(p,arguments);};" +
+       
+        // Introduce the data as local variables using with(){}
+        "with(obj){p.push('" +
+       
+        // Convert the template into pure JavaScript
+        str
+          .replace(/[\r\t\n]/g, " ")
+          .split("<%").join("\t")
+          .replace(/((^|%>)[^\t]*)'/g, "$1\r")
+          .replace(/\t=(.*?)%>/g, "',$1,'")
+          .split("\t").join("');")
+          .split("%>").join("p.push('")
+          .split("\r").join("\\'")
+      + "');}return p.join('');");
+   
+    // Provide some basic currying to the user
+    return data ? fn( data ) : fn;
+  };
+})();
+
+//My script
+
 function verify (evt) 
 {
 	evt.preventDefault();
@@ -7,6 +45,8 @@ function verify (evt)
 	var check = document.getElementById("checkbox");
 	var mesaj = document.getElementById("msg").value;
 	var box = document.getElementById("box");
+	var thanks = document.getElementById("showMessage");
+	var container = document.getElementById("container"); 
 	var errors = [];
 	var text = "";
 
@@ -32,11 +72,12 @@ function verify (evt)
 
 	if(errors.length === 0)
 	{
-		text = "Thank you "+nume+" "+prenume+"!&#10084;"
-		box.innerHTML = text;
-		box.classList.add("disp-inl");
-		document.getElementById("hide1").style.display = "none";
-		document.getElementById("hide2").style.display = "none";
+		//text = tmpl('msg-template', {nume:nume,prenume:prenume});
+		//showMessage.innerHTML = text;
+		text="Thank you "+nume+" "+prenume+"!&#10084"
+		document.getElementById("text-p").innerHTML = text;
+		container.classList.remove("nonMessage");
+		container.classList.add("Message");
 	}
 	else
 	{
