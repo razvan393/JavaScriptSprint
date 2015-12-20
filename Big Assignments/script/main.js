830140d90f2dde171925e4510213395185b09620
razvan393 <razvanghinea05@gmail.com>
2015-12-20 16:19:02 +0200

    BigAssignment

--- Big Assignments/script/main.js
+++ Big Assignments/script/main.js
@@ -6,6 +6,9 @@ var rate = document.getElementById("rate");
 var storage = [];
 var map;
 var geocoder;
+var thName = document.getElementById("h-name");
+var thOras = document.getElementById("h-oras");
+var thRating = document.getElementById("h-rating");
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
@@ -45,7 +48,6 @@ var createRow = function (x) {
     var tr = document.createElement("tr");
     tr.innerHTML = tmpl("tpl", x);
     tableBody.appendChild(tr);
-    createMarker(x.oras);
 };
 
 var getInputs = function(form){
@@ -57,10 +59,12 @@ var getValues = function (form) {
     var name = inputs[0].value;
     var oras = inputs[1].value;
     var rating = parseInt(inputs[2].value,10);
+    var marker = createMarker(oras);
     return {
         name: name,
         oras: oras,
-        rating: rating
+        rating: rating,
+        position: marker
     };
 };
 
@@ -106,6 +110,7 @@ var reloadTable = function () {
     for(var i=0; i<storage.length; i++)
     {
         createRow(storage[i]);
+        reloadMarker(storage[i].position);
     }
 };
 
@@ -117,11 +122,11 @@ var clearInputs = function () {
     document.getElementById("checkbox").checked = false;
 };
 
-var getGrandpa = function (x) {
-    return x.parentNode.parentNode;
+var getFather = function (x) {
+    return x.parentNode;
 };
 
-var getGrandpasPosition = function (x, y) {
+var getPosition = function (x, y) {
     var pos=0;
     for (var i=0; i< y.length; i++)
     {
@@ -149,26 +154,60 @@ var isValidCity = function (address) {
 };
 
 var createMarker  = function (address) {
-    geocoder = new google.maps.Geocoder();
+    var geocoder = new google.maps.Geocoder();
     geocoder.geocode ({'address':address}, function (results, status){
-        if (status == google.maps.GeocoderStatus.OK) {
+        if (status == google.maps.GeocoderStatus.OK)
+        {
             map.setCenter(results[0].geometry.location);
             var marker = new google.maps.Marker({
                 map: map,
                 position: results[0].geometry.location
             });
-            return marker;
+            console.log(typeof marker.position);
+            return results[0].geometry.location;
         }
         else
         {
             console.log("Geocode was not successful for the following reason: " + status);
+            alert("Orasul nu exista!");
             return 0;
         }
     });
 };
 
+var centerMap = function (address) {
+    geocoder = new google.maps.Geocoder();
+    geocoder.geocode({'address':address}, function (results, status) {
+        if (status == google.maps.GeocoderStatus.OK)
+        {
+          map.setCenter(results[0].geometry.location);
+        }
+    });
+};
+
+var reloadMarker = function (pos) {
+    var marker = new google.maps.Marker({
+        map: map,
+        position: pos
+    });
+};
+
+var sort_by = function(field, reverse, primer){
+
+    var key = primer ?
+        function(x) {return primer(x[field])} :
+        function(x) {return x[field]};
+
+    reverse = !reverse ? 1 : -1;
+
+    return function (a, b) {
+        return a = key(a), b = key(b), reverse * ((a > b) - (b > a));
+    }
+}
+
 addBtn.addEventListener("click", function () {
-    if (isAddBtn(event.target)) {
+    if (isAddBtn(event.target))
+    {
         event.preventDefault();
         if(isValid())
         {
@@ -181,14 +220,37 @@ addBtn.addEventListener("click", function () {
 });
 
 tableBody.addEventListener("click", function () {
-    if (isRemoveBtn(event.target)) {
+    if (isRemoveBtn(event.target))
+    {
         event.preventDefault();
-        var grandpa = getGrandpa(event.target);
+        var grandpa = getFather(getFather(event.target));
         var grandpaSiblings = grandpa.parentNode.children;
-        var grandpaPos = getGrandpasPosition(grandpa, grandpaSiblings);
+        var grandpaPos = getPosition(grandpa, grandpaSiblings);
         removeLineStorage(grandpaPos);
         initMap();
         recalculateTotal();
+        reloadTable();
     }
+    else
+    {
+        var father = getFather(getFather(event.target));
+        var fatherSiblings = father.parentNode.children;
+        var fatherPos = getPosition(father, fatherSiblings);
+        centerMap(storage[fatherPos].oras);
+    }
+});
+
+thName.addEventListener("click", function () {
+    storage.sort(sort_by("name", false, function(a){return a.toUpperCase()}));
+    reloadTable();
+});
+
+thOras.addEventListener("click", function () {
+    storage.sort(sort_by("oras", false, function(a){return a.toUpperCase()}));
+    reloadTable();
+});
+
+thRating.addEventListener("click", function () {
+    storage.sort(sort_by("rating", false, parseInt));
     reloadTable();
 });
